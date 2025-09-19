f7b42587339843daabf8408d2a16e972b061f8dd
mohit-kumar-singh55 <mohitkumarsingh12344@gmail.com>
2025-09-19 13:59:05 +0900

    Update: Added intro Scene & impule on brain strike...

--- Assets/Scripts/Sound/SeManager.cs
+++ Assets/Scripts/Sound/SeManager.cs
@@ -1,13 +1,7 @@
-using System;
-using System.Collections;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class SeManager : MonoBehaviour
 {
-    /// <summary>
-    /// サウンドマネージャー
-    /// </summary>
     [SerializeField]
     SoundManager soundManager;
 
